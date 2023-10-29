6d62a49d89f4c070156fe778b5c74067e4c2d2ec
Pavel Nesen <nesen.rabota@gmail.com>
2023-10-30 01:36:02 +0300

    update

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ function navigationBtnAll() {
     navBtnPlus.classList.toggle('navigation__all-plus--active');
     navBtnOpenPlus.classList.toggle('navigation__all-btnbefore--active');
     document.body.classList.toggle('noscroll');
+    navBtnOpen.classList.toggle('marginr');
     margin.classList.toggle('marginr');
   };
   navBtnOpen.onclick = toggleNavAll;
@@ -17,24 +18,86 @@ function navigationBtnAll() {
   navOverlay.onclick = toggleNavAll;
 };
 
-function opacityBtnAll() {
+// function opacityBtnAll() {
+//   setInterval(() => {
+//     if ($(window).scrollTop() > 70 && $(window).scrollTop() < 5000) {
+//       $('.navigation__all').addClass('_active-all')
+//       $('.navigation__all').addClass('navigation__all--active')
+//       } else {
+//       $('.navigation__all').removeClass('_active-all')
+//     }
+//   })
+// }
+
+// opacityBtnAll();
+navigationBtnAll();
+// ======================ACTIVE==========================================================
+
+const elemFront = document.querySelector('.top--front');
+const elemEnd = document.querySelector('.top--end');
+const elemDev = document.querySelector('.bot--developer');
+const elemAbout = document.querySelector('.bot--about');
+const elemNavAll = document.querySelector('.navigation__all');
+const elemFrontEndPlus = document.querySelector('._active');
+
+function scrollBtnAll() {
   setInterval(() => {
-    if ($(window).scrollTop() > 70 && $(window).scrollTop() < 5000) {
-      $('.navigation__all').addClass('active-all')
-      $('.navigation__all').addClass('navigation__all--active')
+    window.addEventListener('scroll', () => {
+      if (elemNavAll && window.scrollY > 70) {
+        elemNavAll.classList.add('_active-all');
+        elemNavAll.classList.add('navigation__all--active');
       } else {
-      $('.navigation__all').removeClass('active-all')
-    }
+        elemNavAll.classList.remove('_active-all');
+      }
+    });
+  },);
+};
+
+scrollBtnAll();
+
+function scrollActive(param, number) {
+    window.addEventListener('scroll', () => {
+      if (param && window.scrollY > number) {
+        param.classList.add('_active');
+      }
+    });
+};
+
+scrollActive(elemFront, 400);
+scrollActive(elemEnd, 400);
+scrollActive(elemDev, 800);
+
+// =======================SCROLL=SMOOTH=========================================================
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substring(1);
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
   })
 }
 
-opacityBtnAll();
-navigationBtnAll();
-// ========================================================
+// =================================================================================
 
+// const anchors = document.querySelectorAll('a[href*="#"]');
 
-
-
+// anchors.forEach(anchor => {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const blockID = anchor.getAttribute('href').substring(1);
+//     const targetElement = document.getElementById(blockID);
+    
+//     if (targetElement) {
+//       targetElement.scrollIntoView({ behavior: 'smooth' });
+//     }
+//   });
+// });
 
 // function a() {
 //   setInterval(() => {
